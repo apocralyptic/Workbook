59f93bf38b7b07b1965fa6489f929c5d7137c672
Nicholas Arcolano <Arcolano@navi.home>
2012-08-12 00:24:33 -0400

    Initial commit.

---
+++ Workbook/Workbook/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Workbook
+//
+//  Created by Nicholas Arcolano on 2012-12-08.
+//  Copyright (c) 2012 Nicholas Arcolano. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
